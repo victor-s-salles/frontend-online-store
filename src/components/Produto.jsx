1ce82ac8b974f19ac70b78fe995f446530563f64
petromamboubee <gm.boubee@gmail.com>
2022-10-07 19:41:34 -0300

    terminei o requisito 7

--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Produto extends React.Component {
   render() {
-    const { productName, productPrice, productImage } = this.props;
+    const { productName, productPrice, productImage, productId } = this.props;
     return (
       <div data-testid="product">
-        <p>{ productName }</p>
-        <p>{ productPrice }</p>
-        <img src={ productImage } alt={ productName } />
-
+        <Link to={ `/produtoDetalhado/${productId}` } data-testid="product-detail-link">
+          <p>{ productName }</p>
+          <p>{ productPrice }</p>
+          <img src={ productImage } alt={ productName } />
+        </Link>
       </div>
     );
   }
@@ -19,6 +21,7 @@ Produto.propTypes = {
   productName: propTypes.string.isRequired,
   productPrice: propTypes.string.isRequired,
   productImage: propTypes.string.isRequired,
+  productId: propTypes.string.isRequired,
 };
 
 export default Produto;
